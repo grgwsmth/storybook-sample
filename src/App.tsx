4c539f7340e0ab94b287b65ef4feae63938cffbb
elmnt <elmnt.gws@gmail.com>
2025-10-08 15:06:09 -0700

    getting started

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [favoriteDogBreed, setFavoriteDogBreed] = useState('')
 
   return (
     <>
@@ -24,6 +27,38 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        
+        <div className="simpleForm">
+          <form>
+            <div>
+              <label htmlFor="fullName">Full Name:</label>
+              <input
+                type="text"
+                id="fullName"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email Address:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="dogBreed">Favorite Dog Breed:</label>
+              <input
+                type="text"
+                id="dogBreed"
+                value={favoriteDogBreed}
+                onChange={(e) => setFavoriteDogBreed(e.target.value)}
+              />
+            </div>
+          </form>
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
